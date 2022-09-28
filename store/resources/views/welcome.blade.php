0355756d394232a62aa109c7c8ab68231f5acfc6
Ahmed-Dokhan111 <ahmaddokhanahmad-2014@outlook.sa>
2022-09-28 08:38:55 +0300

    a

--- store/resources/views/welcome.blade.php
+++ store/resources/views/welcome.blade.php
@@ -121,6 +121,10 @@
                             </a>
                         </div>
                     </div>
+                    <div class="card">
+                        <p>
+                        ahemd</p>  
+                    </div>
 
                     <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
